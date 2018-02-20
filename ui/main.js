b8781a5ad35dcec1fc0d5c8373de43411b424d4e
janapareddykanthi <nagalaxmi1001@gmail.com>
2018-02-20 22:01:24 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,8 @@
 console.log('Loaded!');
 var element=document.getElementById('main-txt');
 element.innerHTML='kanthi';
+var img=document.getElementById('money');
+img.onClick=function() 
+{
+    img.style.marginleft='120px';
+};
